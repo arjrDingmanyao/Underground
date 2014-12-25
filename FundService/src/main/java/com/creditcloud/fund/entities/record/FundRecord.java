f6678faa002f21cbec6494bb59ad9d13e55da352
arjrWangkun <arjrWangkun@anrunjinrong>
2014-12-25 11:30:24 +0800

    后台提历史记录
    后台提历史记录

--- FundService/src/main/java/com/creditcloud/fund/entities/record/FundRecord.java
+++ FundService/src/main/java/com/creditcloud/fund/entities/record/FundRecord.java
@@ -12,6 +12,7 @@ import com.creditcloud.fund.model.enums.FundRecordOperation;
 import com.creditcloud.fund.model.enums.FundRecordStatus;
 import com.creditcloud.fund.model.enums.FundRecordType;
 import java.math.BigDecimal;
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorValue;
@@ -25,7 +26,12 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.Setter;
 import org.eclipse.persistence.annotations.Index;
 
 /**
@@ -38,43 +44,43 @@ import org.eclipse.persistence.annotations.Index;
 @DiscriminatorValue("FundRecord")
 @DiscriminatorColumn(name = "DTYPE")
 @Table(name = "TB_FUND_RECORD",
-       uniqueConstraints = {
-           @UniqueConstraint(columnNames = {"USER_ID", "type", "orderId"})})
+	uniqueConstraints = {
+	    @UniqueConstraint(columnNames = {"USER_ID", "type", "orderId"})})
 @NamedQueries({
     /**
      * get query
      */
     @NamedQuery(name = "FundRecord.getByOrderId",
-                query = "select fr from FundRecord fr where fr.fund.userId = :userId and fr.type = :type and fr.orderId = :orderId"),
+	    query = "select fr from FundRecord fr where fr.fund.userId = :userId and fr.type = :type and fr.orderId = :orderId"),
     @NamedQuery(name = "FundRecord.getByOrderIdAndType",
-                query = "select fr from FundRecord fr where fr.type = :type and fr.orderId = :orderId order by fr.timeRecorded DESC"),
+	    query = "select fr from FundRecord fr where fr.type = :type and fr.orderId = :orderId order by fr.timeRecorded DESC"),
     /**
      * count query
      */
     @NamedQuery(name = "FundRecord.countByUserAndTypeAndOperationAndStatus",
-                query = "select count(fr) from FundRecord fr where fr.fund.userId = :userId and fr.type in :typeList and fr.operation in :operationList and fr.status in :statusList"),
+	    query = "select count(fr) from FundRecord fr where fr.fund.userId = :userId and fr.type in :typeList and fr.operation in :operationList and fr.status in :statusList"),
     @NamedQuery(name = "FundRecord.countByType",
-                query = "select count(fr) from FundRecord fr where fr.type in :typeList"),
+	    query = "select count(fr) from FundRecord fr where fr.type in :typeList"),
     @NamedQuery(name = "FundRecord.countByUserAndTypeAndTime",
-                query = "select count(fr) from FundRecord fr where fr.fund.userId = :userId and fr.timeRecorded between :from and :to and fr.type in :typeList"),
+	    query = "select count(fr) from FundRecord fr where fr.fund.userId = :userId and fr.timeRecorded between :from and :to and fr.type in :typeList"),
     @NamedQuery(name = "FundRecord.countByUserAndTypeAndOperationAndStatusAndTime",
-                query = "select count(fr) from FundRecord fr where fr.fund.userId = :userId and fr.timeRecorded between :from and :to and fr.type in :typeList and fr.operation in :operationList and fr.status in :statusList"),
+	    query = "select count(fr) from FundRecord fr where fr.fund.userId = :userId and fr.timeRecorded between :from and :to and fr.type in :typeList and fr.operation in :operationList and fr.status in :statusList"),
     /**
      * list query
      */
     @NamedQuery(name = "FundRecord.listByUserAndTypeAndTime",
-                query = "select fr from FundRecord fr where fr.fund.userId = :userId and fr.timeRecorded between :from and :to and fr.type in :typeList order by fr.timeRecorded DESC"),
+	    query = "select fr from FundRecord fr where fr.fund.userId = :userId and fr.timeRecorded between :from and :to and fr.type in :typeList order by fr.timeRecorded DESC"),
     @NamedQuery(name = "FundRecord.listByType",
-                query = "select fr from FundRecord fr where fr.type in :typeList order by fr.timeRecorded ASC"),
+	    query = "select fr from FundRecord fr where fr.type in :typeList order by fr.timeRecorded ASC"),
     @NamedQuery(name = "FundRecord.listByUserAndTypeAndOperationAndStatus",
-                query = "select fr from FundRecord fr where fr.fund.userId = :userId and fr.type in :typeList and fr.operation in :operationList and fr.status in :statusList order by fr.timeRecorded DESC"),
+	    query = "select fr from FundRecord fr where fr.fund.userId = :userId and fr.type in :typeList and fr.operation in :operationList and fr.status in :statusList order by fr.timeRecorded DESC"),
     @NamedQuery(name = "FundRecord.listByUserAndTypeAndOperationAndStatusAndTime",
-                query = "select fr from FundRecord fr where fr.fund.userId = :userId and fr.timeRecorded between :from and :to and fr.type in :typeList and fr.operation in :operationList and fr.status in :statusList order by fr.timeRecorded DESC"),
+	    query = "select fr from FundRecord fr where fr.fund.userId = :userId and fr.timeRecorded between :from and :to and fr.type in :typeList and fr.operation in :operationList and fr.status in :statusList order by fr.timeRecorded DESC"),
     /**
      * update
      */
     @NamedQuery(name = "FundRecord.markStatus",
-                query = "update FundRecord fr set fr.status = :status where fr.id = :id")
+	    query = "update FundRecord fr set fr.status = :status where fr.id = :id")
 })
 public class FundRecord extends RecordScopeEntity {
 
@@ -122,8 +128,8 @@ public class FundRecord extends RecordScopeEntity {
      * 金额
      */
     @Column(nullable = false,
-            precision = 15,
-            scale = 2)
+	    precision = 15,
+	    scale = 2)
     protected BigDecimal amount;
 
     /*
@@ -143,108 +149,147 @@ public class FundRecord extends RecordScopeEntity {
      */
     protected String description;
 
+    /**
+     * 给fundWithdraw（提现使用）
+     */
+    @Transient
+    @Getter
+    @Setter
+    protected BigDecimal withdrawFee;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = true)
+    @Getter
+    @Setter
+    protected Date approveDateTime;
+
     public FundRecord(UserFund fund,
-                      FundAccount account,
-                      RealmEntity entity,
-                      FundRecordType type,
-                      FundRecordStatus status,
-                      FundRecordOperation operation,
-                      BigDecimal amount,
-                      String orderId,
-                      String transactionId,
-                      String description) {
-        this.fund = fund;
-        this.account = account;
-        this.entity = entity;
-        this.type = type;
-        this.status = status;
-        this.operation = operation;
-        this.amount = amount;
-        this.orderId = orderId;
-        this.transactionId = transactionId;
-        this.description = description;
+	    FundAccount account,
+	    RealmEntity entity,
+	    FundRecordType type,
+	    FundRecordStatus status,
+	    FundRecordOperation operation,
+	    BigDecimal amount,
+	    String orderId,
+	    String transactionId,
+	    String description) {
+	this.fund = fund;
+	this.account = account;
+	this.entity = entity;
+	this.type = type;
+	this.status = status;
+	this.operation = operation;
+	this.amount = amount;
+	this.orderId = orderId;
+	this.transactionId = transactionId;
+	this.description = description;
+    }
+
+    public FundRecord(UserFund fund,
+	    FundAccount account,
+	    RealmEntity entity,
+	    FundRecordType type,
+	    FundRecordStatus status,
+	    FundRecordOperation operation,
+	    BigDecimal amount,
+	    String orderId,
+	    String transactionId,
+	    String description, BigDecimal withdrawFee, Date approveDateTime) {
+	this.fund = fund;
+	this.account = account;
+	this.entity = entity;
+	this.type = type;
+	this.status = status;
+	this.operation = operation;
+	this.amount = amount;
+	this.orderId = orderId;
+	this.transactionId = transactionId;
+	this.description = description;
+	this.withdrawFee = withdrawFee;
+	this.approveDateTime = approveDateTime;
     }
 
     public FundRecord() {
     }
 
     public UserFund getFund() {
-        return fund;
+	return fund;
     }
 
     public RealmEntity getEntity() {
-        return entity;
+	return entity;
     }
 
     public FundRecordType getType() {
-        return type;
+	return type;
     }
 
     public FundRecordStatus getStatus() {
-        return status;
+	return status;
     }
 
     public FundRecordOperation getOperation() {
-        return operation;
+	return operation;
     }
 
     public BigDecimal getAmount() {
-        return amount;
+	return amount;
     }
 
     public String getOrderId() {
-        return orderId;
+	return orderId;
     }
 
     public String getTransactionId() {
-        return transactionId;
+	return transactionId;
     }
 
     public String getDescription() {
-        return description;
+	return description;
     }
 
     public void setFund(UserFund fund) {
-        this.fund = fund;
+	this.fund = fund;
     }
 
     public void setEntity(RealmEntity entity) {
-        this.entity = entity;
+	this.entity = entity;
     }
 
     public void setType(FundRecordType type) {
-        this.type = type;
+	this.type = type;
     }
 
     public void setStatus(FundRecordStatus status) {
-        this.status = status;
+	this.status = status;
     }
 
     public void setOperation(FundRecordOperation operation) {
-        this.operation = operation;
+	this.operation = operation;
     }
 
     public void setAmount(BigDecimal amount) {
-        this.amount = amount;
+	this.amount = amount;
     }
 
     public void setOrderId(String orderId) {
-        this.orderId = orderId;
+	this.orderId = orderId;
     }
 
     public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
+	this.transactionId = transactionId;
     }
 
     public void setDescription(String description) {
-        this.description = description;
+	this.description = description;
     }
 
     public FundAccount getAccount() {
-        return account;
+	return account;
     }
 
     public void setAccount(FundAccount account) {
-        this.account = account;
+	this.account = account;
     }
+
 }
