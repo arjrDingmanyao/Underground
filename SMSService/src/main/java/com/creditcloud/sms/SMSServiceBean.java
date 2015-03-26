701bd27f67dae17cf70d8991b7b907addf1704e7
guanyi <guanyi@anrunjinrong.com>
2015-03-26 10:27:25 +0800

    短信黑名单
    短信黑名单

--- SMSService/src/main/java/com/creditcloud/sms/SMSServiceBean.java
+++ SMSService/src/main/java/com/creditcloud/sms/SMSServiceBean.java
@@ -84,7 +84,7 @@ public class SMSServiceBean implements SMSService {
         boolean flag = true;
         List<SMSBlackList> smsBlackList = smsBlackListDao.findAll();
         for(SMSBlackList sb : smsBlackList){
-            if(mobile.equals(sb.getNumber())){
+            if(mobile.trim().equals(sb.getNumber().trim())){
                 flag = false;
                 logger.info(mobile+"in blackList.");
             }else{
